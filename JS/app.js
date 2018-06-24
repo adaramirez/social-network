a90a63d85c78cd38337d4d44c04665aac5814801
DolceLemus <dulce.lemus.lara@gmail.com>
2018-06-23 19:39:13 -0500

    se logra hacer la vista previa de la imagen, haciendo template para el post

--- JS/app.js
+++ JS/app.js
@@ -1,11 +1,11 @@
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyDwCaomDJgBm4WeP2l-bG3Pi3w9qmm_Tug",
-  authDomain: "social-network-turism.firebaseapp.com",
-  databaseURL: "https://social-network-turism.firebaseio.com",
-  projectId: "social-network-turism",
-  storageBucket: "",
-  messagingSenderId: "162849485241"
+    apiKey: "AIzaSyDwCaomDJgBm4WeP2l-bG3Pi3w9qmm_Tug",
+    authDomain: "social-network-turism.firebaseapp.com",
+    databaseURL: "https://social-network-turism.firebaseio.com",
+    projectId: "social-network-turism",
+    storageBucket: "",
+    messagingSenderId: "162849485241"
 };
 firebase.initializeApp(config);
 
@@ -13,16 +13,19 @@ var dbFB = firebase.database().ref().child('post');
 
 // upload image posting
 $(document).ready(function() {
-		function readURL(input) {
-		    if (input.files && input.files[0]) {
-		        var reader = new FileReader();
-		        reader.onload = function (e) {
-		            $('#img-upload').attr('src', e.target.result);
-		        }
-		        reader.readAsDataURL(input.files[0]);
-		    }
-		}
-		$("#browse-img").change(function(){
-		    readURL(this);
-		});
-	});
+
+    // atributo de la ruta de la imagen
+    function browseImage(inputPhoto) {
+        if (inputPhoto.files && inputPhoto.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                $('#img-upload').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(inputPhoto.files[0]);
+        }
+    }
+
+    $("#browse-img").change(function() {
+        browseImage(this);
+    });
+});
